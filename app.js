36b2d54f3f32a6b2d5390cfc9f2f98c216c791a2
Ian Macalinao <ian@giza.us>
2014-09-13 00:37:20 -0500

    Make oauth echo

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.route('/auth').get(function(req, res) {
   res.redirect('https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=jmPj4TUbUIw&redirect_uri=http://fitfood.herokuapp.com/oauth&scope=basic_read extended_read location_read friends_read mood_read mood_write move_read move_write sleep_read sleep_write meal_read meal_write weight_reada weight_write generic_event_read generic_event_write');
 });
 
+app.route('/oauth').get(function(req, res) {
+  res.send(req.query);
+});
+
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
