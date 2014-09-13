1ac2e6c38c1a0839ca0593d1275abb345d8935c9
Ian Macalinao <ian@giza.us>
2014-09-13 00:42:48 -0500

    improve readability

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ app.route('/').all(function(req, res) {
 });
 
 app.route('/auth').get(function(req, res) {
-  res.redirect('https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=jmPj4TUbUIw&redirect_uri=http://fitfood.herokuapp.com/oauth&scope=basic_read extended_read location_read friends_read mood_read mood_write move_read move_write sleep_read sleep_write meal_read meal_write weight_reada weight_write generic_event_read generic_event_write');
+  res.redirect(['https://jawbone.com/auth/oauth2/auth',
+    '?response_type=code&client_id=jmPj4TUbUIw',
+    '&redirect_uri=http://fitfood.herokuapp.com/oauth',
+    '&scope=basic_read extended_read location_read ',
+    'friends_read mood_read mood_write move_read move_write ',
+    'sleep_read sleep_write meal_read meal_write weight_read ',
+    'weight_write generic_event_read generic_event_write'].join(''));
 });
 
 app.route('/oauth').get(function(req, res) {
