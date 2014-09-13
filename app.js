4d576b71aef598069f2d2f9542077a55b3f95f52
Ian Macalinao <ian@giza.us>
2014-09-13 00:36:48 -0500

    Make auth route work

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.route('/').all(function(req, res) {
   res.send('Hello');
 });
 
+app.route('/auth').get(function(req, res) {
+  res.redirect('https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=jmPj4TUbUIw&redirect_uri=http://fitfood.herokuapp.com/oauth&scope=basic_read extended_read location_read friends_read mood_read mood_write move_read move_write sleep_read sleep_write meal_read meal_write weight_reada weight_write generic_event_read generic_event_write');
+});
+
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
